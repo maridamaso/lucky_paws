f50ad83728ad93986038e650d70a39886bc04ab5
Marinna Damaso <marinnadamaso@gmail.com>
2023-05-10 18:22:17 -0300

    criação da tela de resultados
    Adicionei a tela de resultados, tirando o alerta que estava desde então sendo utilizado

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,3 +1,4 @@
+// Função que troca entre modo claro e modo escuro
 function trocaModo() 
 {
     let modoAtual = document.body.classList.contains('modoClaro') ? 'modoClaro' : 'modoEscuro'; 
@@ -19,6 +20,9 @@ function trocaModo()
     imagemModo.setAttribute('src', novoModo === 'modoClaro' ? 'Imagens/sun.png' : 'Imagens/moon.png');
     //Mudando o caminho da imagem
   }
+
+
+// Função de sorteio de números 
 function sortear(){
   let quantNum  = parseInt(document.querySelector('#quantNum').value);
   let intervalo01  = parseInt(document.querySelector('#intervalo01').value);
@@ -31,12 +35,13 @@ function sortear(){
     if(intervalo01 > intervalo02)
     {
       numeroAleatorio = Math.floor(Math.random() * (intervalo01 - intervalo02 + 1)) + intervalo02;
-		  alert("O numero escolhido foi:" + numeroAleatorio)
+      resultado(numeroAleatorio);
+
     }
     else
     {
       numeroAleatorio = Math.floor(Math.random() * (intervalo02 - intervalo01 + 1)) + intervalo01;
-		  alert("O numero escolhido foi:" + numeroAleatorio)
+      resultado(numeroAleatorio);
     }
   }
   else
@@ -47,7 +52,7 @@ function sortear(){
         let numero = Math.floor(Math.random() * (intervalo01 - intervalo02 + 1)) + intervalo02;
         numeroAleatorios.push(numero);
       }
-      alert('Os números escolhidos foram: ' + numeroAleatorios);
+      resultado(numeroAleatorios);
     }
     else
     {
@@ -55,20 +60,49 @@ function sortear(){
         let numero = Math.floor(Math.random() * (intervalo02 - intervalo01 + 1)) + intervalo01;
         numeroAleatorios.push(numero);
       }
-      alert('Os números escolhidos foram: ' + numeroAleatorios);
+      resultado(numeroAleatorios);
     }
   }
 }
+
+
+// Função que troca o valor para números ou número de acordo com a quant que a pessoa digitar
 function trocaValor()
 {
   let valor = parseInt(document.querySelector('#quantNum').value);
   let texto = document.querySelector("#texto");
+  let trocaNome = document.querySelector('#singular');
   if (valor === 1)
   {
     texto.innerHTML = 'número';
+    trocaNome.innerHTML = 'O NÚMERO SORTEADO FOI';
   }
   else 
   {
     texto.innerHTML = 'números';
+    trocaNome.innerHTML = 'OS NÚMEROS SORTEADOS FORAM';
   }
+}
+
+
+//Função que troca a visualização da página de Conteudo para Resposta
+function resultado(valor){
+  let conteudoNumeros = document.querySelector('.conteudoSorteioNumeros');
+  let resultadoNumeros = document.querySelector('.resultadoSorteioNumeros');
+  let valorResultado = document.querySelector('#resultado');
+  let trocaNome = document.querySelector('#singular');
+
+  conteudoNumeros.style.display = 'none';
+  resultadoNumeros.style.display = 'block';
+
+  valorResultado.textContent = valor;
+}
+
+// Função que volta a visualização para a tela de sorteio dde numeros
+function voltar(){
+  let conteudoNumeros = document.querySelector('.conteudoSorteioNumeros');
+  let resultadoNumeros = document.querySelector('.resultadoSorteioNumeros');
+
+  conteudoNumeros.style.display = 'block';
+  resultadoNumeros.style.display = 'none';
 }
